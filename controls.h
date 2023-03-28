b666584e6eaa4c233d258252597f968fe64eaf64
kevinsilbernagel <kevinsil@stud.ntnu.no>
2023-03-28 16:37:15 +0200

    lagt til kommentarer

--- controls.h
+++ controls.h
@@ -2,127 +2,158 @@
 #define THREEPP_VCPKG_TEST_CONTROLS_H
 
 #include "chessBoardGeometry.h"
-
-#include <GLFW/glfw3.h>
-#include <threepp/controls/OrbitControls.hpp>
-#include <threepp/threepp.hpp>
-#include <functional>
-#include <threepp/core/Raycaster.hpp>
-#include <threepp/objects/Mesh.hpp>
-#include <threepp/Canvas.hpp>
-#include <memory>
+#include "threepp/controls/OrbitControls.hpp"
+#include "threepp/threepp.hpp"
+#include "functional"
+#include "threepp/core/Raycaster.hpp"
+#include "threepp/objects/Mesh.hpp"
+#include "threepp/Canvas.hpp"
+#include "memory"
+#include "utility"
+#include "iostream"
 
 using namespace threepp;
 
+//lager klasse for flytting av sjakkbrikker (ikke helt implementert enda, men på saken:))
 class Controls {
 public:
-    Controls(std::shared_ptr<Camera> camera, std::shared_ptr<Chessboard> chessboard, std::shared_ptr<Canvas> canvas, std::shared_ptr<Scene> scene)
-            : camera_(std::move(camera)), chessboard_(std::move(chessboard)), canvas_(std::move(canvas)), scene_(std::move(scene))
+    Controls(std::shared_ptr<PerspectiveCamera>& camera, Canvas& canvas, std::shared_ptr<Scene> scene)
+            : camera_(camera), canvas_(&canvas), scene_(std::move(scene))
     {
-        // Get the GLFW window
-        GLFWwindow* window = static_cast<GLFWwindow*>(canvas_->window_ptr());
-
-
-        // Set the user pointer to this class
-        glfwSetWindowUserPointer(window, this);
-
-        // Register the mouse event handlers
-        glfwSetMouseButtonCallback(window, [](GLFWwindow* window, int button, int action, int mods) {
-            Controls* controls = static_cast<Controls*>(glfwGetWindowUserPointer(window));
-
-            if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
-                double x, y;
-                glfwGetCursorPos(window, &x, &y);
-                controls->onMouseDown(Vector2(static_cast<float>(x), static_cast<float>(y)));
-            } else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE) {
-                double x, y;
-                glfwGetCursorPos(window, &x, &y);
-                controls->onMouseUp(Vector2(static_cast<float>(x), static_cast<float>(y)));
+        // Find the chessboard mesh
+        for (const auto& object : scene_->children) {
+            if (auto mesh = std::dynamic_pointer_cast<Mesh>(object); mesh && mesh->name == "chessboard") {
+                chessboard_ = mesh;
+                break;
             }
-        });
-
-        glfwSetCursorPosCallback(window, [](GLFWwindow* window, double x, double y) {
-            Controls* controls = static_cast<Controls*>(glfwGetWindowUserPointer(window));
-            controls->onMouseMove(Vector2(static_cast<float>(x), static_cast<float>(y)));
-        });
+        }
     }
 
-    void onMouseDown(const Vector2 &pos) {
-        mouseDown = true;
-        mousePosition = pos;
+    std::shared_ptr<Mesh> selectedMesh_ = nullptr;
+    Mesh* selectedRawMesh_ = nullptr;
 
-        auto raycaster = std::make_shared<Raycaster>(camera_->position, Vector3(0, 0, -1));
+    // funksjon for håndtering av trykt museknapp
+    void onMouseDown(const Vector2& mousePosition) {
+        mouseDown_ = true;
+        mousePosition_ = mousePosition;
+        auto intersection = getIntersection(mousePosition_);
+        if (intersection && intersection->object->parent->is<Mesh>()) {
+            auto meshName = intersection->object->parent->name;
+            if (meshName.find("chess_piece") != std::string::npos) {
+                selectedRawMesh_ = dynamic_cast<Mesh*>(intersection->object->parent);
+                dragOffset_ = selectedRawMesh_->position - intersection->point;
+                std::cout << "Selected mesh: " << selectedRawMesh_->name << std::endl;
+            }
+        } else {
+            selectedRawMesh_ = nullptr;
+        }
+    }
+
+    //håndtering for flytting av mus når museknapp er nedtrykt
+    void onMouseMove(const Vector2& pos)
+    {
+        if (!selectedRawMesh_ || !mouseDown_)
+            return;
+
+        auto intersection = getIntersection(pos);
+        if (intersection) {
+            auto newPosition = intersection->point + dragOffset_;
+            selectedRawMesh_->position = newPosition; // update the position of the selected mesh
+            std::cout << "Moved mesh " << selectedRawMesh_->name << " to position (" << newPosition.x << ", " << newPosition.y << ", " << newPosition.z << ")" << std::endl;
+        }
+    }
+
+    //håndtering for flytting av mus når museknapp er nedtrykt
+    void moveSelectedMeshTo(const Vector3& position) {
+        if (selectedRawMesh_) {
+            auto newPosition = Vector3(position.x, selectedRawMesh_->position.y, position.z);
+            selectedRawMesh_->position.set(newPosition.x, newPosition.y, newPosition.z); // update the position of the selected mesh
+            std::cout << "Moved mesh " << selectedRawMesh_->name << " to position (" << newPosition.x << ", " << newPosition.y << ", " << newPosition.z << ")" << std::endl;
+        }
+    }
+
+    //håndtering av når museknapp blir sluppet
+    void onMouseUp() {
+        if (mouseDown_) {
+            mouseDown_ = false;
+            auto intersection = getIntersection(mousePosition_);
+            if (intersection) {
+                Vector3 newPosition(selectedRawMesh_->position.x, selectedRawMesh_->position.y, selectedRawMesh_->position.z);
+                moveSelectedMeshTo(newPosition);
+                std::cout << "Released mesh " << selectedRawMesh_->name << std::endl;
+            }
+            selectedRawMesh_ = nullptr;
+        }
+    }
+//private medlemmer
+private:
+    std::shared_ptr<Camera> camera_;
+    std::shared_ptr<Mesh> chessboard_;
+    Canvas* canvas_;
+    std::shared_ptr<Scene> scene_;
+
+    bool mouseDown_ = false;
+    Vector2 mousePosition_;
+    Vector3 dragOffset_;
+
+    //håndtering av raycast og intersections ifhold til chesspieces
+    std::optional<Intersection> getIntersection(const Vector2& pos) {
+        auto raycaster = std::make_shared<Raycaster>();
         raycaster->setFromCamera(pos, camera_);
 
-        auto &objects = scene_->children;
+        std::cout << "Raycaster:" << std::endl;
+        std::cout << "  Origin: (" << raycaster->ray.origin.x << ", " << raycaster->ray.origin.y << ", "
+                  << raycaster->ray.origin.z << ")" << std::endl;
+        std::cout << "  Direction: (" << raycaster->ray.direction.x << ", " << raycaster->ray.direction.y << ", "
+                  << raycaster->ray.direction.z << ")" << std::endl;
 
-        for (auto &object: objects) {
-            auto mesh = std::dynamic_pointer_cast<Mesh>(object);
-            if (mesh && mesh->name.substr(0, 11) == "chess_piece") {
-                auto intersects = raycaster->intersectObject(mesh.get());
-                if (!intersects.empty()) {
-                    selectedMesh = mesh.get();
-                    dragStartPosition = pos;
-                    dragOffset = selectedMesh->position - intersects[0].point;
-                    break;
+
+        std::vector<std::shared_ptr<Mesh>> chessPieces;
+        for (const auto &object: scene_->children) {
+            if (auto mesh = std::dynamic_pointer_cast<Mesh>(object); mesh) {
+                // Check if the mesh is a chess piece
+                if (mesh->name.find("chess_piece") != std::string::npos ||
+                    mesh->name.find("White") == 0 ||
+                    mesh->name.find("Black") == 0) {
+                    chessPieces.push_back(mesh);
+                    std::cout << mesh->name << " found" << std::endl;
                 }
             }
         }
-    }
 
-    void onMouseMove(const Vector2 &pos) {
-        if (!selectedMesh || !mouseDown) return;
+        std::shared_ptr<Mesh> closestChessPiece = nullptr;
+        Intersection closestIntersection;
+        double closestDistance = std::numeric_limits<double>::max();
 
-        auto intersection = getIntersection(pos);
-        if (intersection) {
-            auto newPosition = intersection->point + dragOffset;
-            selectedMesh->position.setX(newPosition.x);
-            selectedMesh->position.setZ(newPosition.z);
-        }
-    }
-
-    void onMouseUp(const Vector2 &pos) {
-        mouseDown = false;
-        if (!selectedMesh) return;
-
-        auto intersection = getIntersection(pos);
-        if (intersection) {
-            auto newPosition = intersection->point + dragOffset;
-            selectedMesh->position.setX(newPosition.x);
-            selectedMesh->position.setZ(newPosition.z);
+        for (const auto &chessPiece: chessPieces) {
+            auto intersects = raycaster->intersectObject(chessPiece.get());
+            for (const auto &intersection: intersects) {
+                if (intersection.distance < closestDistance) {
+                    closestIntersection = intersection;
+                    closestDistance = intersection.distance;
+                    closestChessPiece = chessPiece;
+                }
+            }
         }
 
-        // Perform any necessary actions after piece has been moved, such as checking for a valid move or updating the game state
-        chessboard_->onPieceMoved(selectedMesh);
-
-        // Clear the selected mesh
-        selectedMesh = nullptr;
-    }
-
-private:
-    std::shared_ptr<Camera> camera_;
-    std::shared_ptr<Chessboard> chessboard_;
-    std::shared_ptr<Canvas> canvas_;
-    std::shared_ptr<Scene> scene_;
-
-    Mesh *selectedMesh = nullptr;
-    bool mouseDown = false;
-    Vector2 mousePosition;
-    Vector2 dragStartPosition;
-    Vector3 dragOffset;
-
-    std::optional<Intersection> getIntersection(const Vector2 &pos) {
-        auto raycaster = std::make_shared<Raycaster>(camera_->position, Vector3(0, 0, 1));
-        raycaster->setFromCamera(pos, camera_);
-        auto intersects = raycaster->intersectObject(chessboard_->getBoardMesh().get());
-
-        if (!intersects.empty()) {
-            return intersects[0];
+        if (closestChessPiece) {
+            std::cout << "Closest chess piece: " << closestChessPiece->name << std::endl;
         } else {
-            return std::nullopt;
+            std::cout << "No chess piece found." << std::endl;
         }
+
+        if (closestDistance < std::numeric_limits<double>::max()) {
+            std::cout << "Closest intersection:" << std::endl;
+            std::cout << "  Distance: " << closestIntersection.distance << std::endl;
+            std::cout << "  Point: (" << closestIntersection.point.x << ", " << closestIntersection.point.y << ", "
+                      << closestIntersection.point.z << ")" << std::endl;
+            std::cout << "  Normal: (" << closestIntersection.face->normal.x << ", "
+                      << closestIntersection.face->normal.y << ", " << closestIntersection.face->normal.z << ")"
+                      << std::endl;
+            return closestIntersection;
+        }
+
+        return std::nullopt;
     }
 };
-
 #endif //THREEPP_VCPKG_TEST_CONTROLS_H
-
